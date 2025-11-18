59929d672ff7401eff141af9be8302ef501591ae
Destruidoss <natal_sasuke@outlook.com>
2025-11-18 11:09:42 +0000

    commit employess_controller

--- fundamentals/app/controllers/employees_controller.rb
+++ fundamentals/app/controllers/employees_controller.rb
@@ -1,8 +1,10 @@
 class EmployeesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
   def index
     @employees = Employee.all 
     @hired_this_month = Employee.where(
-      hired_at: Date.current.beginning_of_month..Date.current.end_toof_month
+      hired_at: Date.current.beginning_of_month..Date.current.end_of_month
     )
     @fired_this_month = Employee.where.not(fired_at: nil).where(fired_at: Date.current.beginning_of_month..Date.current.end_of_month)
   end
