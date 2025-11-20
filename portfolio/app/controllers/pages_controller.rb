8508f3c5870177a31e00546453aec9085d52b757
Destruidoss <natal_sasuke@outlook.com>
2025-11-20 15:13:46 +0000

    commit portfolio finish

--- portfolio/app/controllers/pages_controller.rb
+++ portfolio/app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
+  
+  def welcome
+
+  end
+  
   def portfolio
     @projetos = [
-      {titulo: "Blog com Rails, teste1"}
-      {titulo: "Loja Virtual, teste2"}
-      {titulo: "Todo List",}
-
-
+      { titulo: "Blog com Rails", descricao: "Um blog completo com posts e comentários", link: "https://meu-blog.herokuapp.com", imagem: "https://via.placeholder.com/400x300" },
+      { titulo: "Loja Virtual", descricao: "E-commerce com carrinho e pagamento", link: "#", imagem: "https://via.placeholder.com/400x300" },
+      { titulo: "Todo List", descricao: "Aplicativo de tarefas com drag and drop", link: "#", imagem: "https://via.placeholder.com/400x300" }
     ]
-
-
   end
 
   def about
-  
+
+  end
+
+  def contact
+
   end
 end
